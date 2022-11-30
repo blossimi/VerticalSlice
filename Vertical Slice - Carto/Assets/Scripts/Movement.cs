90ad2c3269f855694fb40ba2408210c16bce478b
AsBas <90893787+sappieman@users.noreply.github.com>
2022-11-30 09:52:15 +0100

    idk

--- Vertical Slice - Carto/Assets/Scripts/Movement.cs
+++ Vertical Slice - Carto/Assets/Scripts/Movement.cs
@@ -3,38 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class playerMovement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
 
-   
-
-    Vector2 movement;
-    Vector2 mousePos;
-
-
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Camera cam;
-   
-
+    private void Start()
+    {
+        
+    }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
+       
+        
        
 
     }
 
-    void FixedUpdate()
-    {
-        Vector2 lookDirection = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
-
-
-    }
+   
 
     
 
