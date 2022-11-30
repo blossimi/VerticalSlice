03fa5324405d7ed4e6ea0a7ad9d78685c4c00350
AsBas <90893787+sappieman@users.noreply.github.com>
2022-11-30 11:29:45 +0100

    movement

--- Vertical Slice - Carto/Assets/Scripts/Movement.cs
+++ Vertical Slice - Carto/Assets/Scripts/Movement.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
+    public GameObject Player;
+    private Rigidbody2D rb;
+    public float movementSpeed;
     private void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-       
         
-       
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            transform.position += new Vector3(-movementSpeed, 0, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.position += new Vector3(movementSpeed, 0, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.position += new Vector3(0, 0, movementSpeed) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            transform.position += new Vector3(0, 0, -movementSpeed) * Time.deltaTime;
+        }
+
+
 
     }
 
