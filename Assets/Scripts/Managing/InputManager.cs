b48c3e65fcadb8dc41e3543602064c7a347d1674
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-12-14 12:28:25 +0100

    added inputmanager to make UI (in)visible

---
+++ Assets/Scripts/Managing/InputManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public KeyCode UI;
+    public GameObject UICanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UICanvas = GameObject.Find("Canvas");
+        UICanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(UI))
+        {
+            if (UICanvas.activeInHierarchy) //If UI is ON:
+            {
+                //DISABLE UI
+                UICanvas.SetActive(false);
+            }
+            else if (!UICanvas.activeInHierarchy) //If UI is OFF:
+            {
+                //ENABLE UI
+                UICanvas.SetActive(true);
+            }
+        }
+    }
+}
