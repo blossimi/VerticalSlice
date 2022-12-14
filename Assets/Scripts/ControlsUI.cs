aa286500d6f6ec9522488d3fec24993da240dfb7
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-12-14 10:23:59 +0100

    Added buttons

---
+++ Assets/Scripts/ControlsUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsUI : MonoBehaviour
+{
+    public GameObject A_pickup;
+    public GameObject A_putdown;
+    public GameObject LR;
+    public GameObject B;
+    public GameObject Y;
+    public GameObject X;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   /* void RenderInOut(GameObject button, bool render)
+    {
+        if (render == true)
+        {
+            button
+        }
+    }*/
+}
