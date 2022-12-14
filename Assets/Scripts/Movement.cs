2093376e5d4e368d3fcbafd56c7850b0f8f09999
AsBas <90893787+sappieman@users.noreply.github.com>
2022-12-14 10:46:20 +0100

    game music

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -41,14 +41,39 @@ public class Movement : MonoBehaviour
 
             transform.position += new Vector3(0, 0, -movementSpeed) * Time.deltaTime;
         }
+        //
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+        {
+
+            transform.position += new Vector3(-movementSpeed, 0, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.position += new Vector3(movementSpeed, 0, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.position += new Vector3(0, 0, movementSpeed) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            transform.position += new Vector3(0, 0, -movementSpeed) * Time.deltaTime;
+        }
+
 
 
 
     }
 
-   
 
-    
+
+
 
 
 
