3847e8198c75a6db09dc14cc537a48a059a2fa1c
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-12-21 10:42:18 +0100

    Added basic version of dynamic border rendering on tiles in UI

--- Assets/Scripts/UITileController.cs
+++ Assets/Scripts/UITileController.cs
@@ -11,13 +11,26 @@ public class UITileController : MonoBehaviour
     public List<GameObject> chunksInWorld;
     public GameObject baseTileGroup;
 
+    //Borders
+    [Header("Borders")]
+    public GameObject top;
+    public GameObject bottom;
+    public GameObject left;
+    public GameObject right;
+    [Space]
+    public Sprite defaultSprite;
+    public GameObject[] uiTiles;
+
     private GameObject canvas;
+    private Color defaultColor;
 
     // Start is called before the first frame update
     void Start()
     {
         thisChunk = gameObject.GetComponent<Chunk>();
         canvas = GameObject.Find("Canvas");
+        uiTiles = GameObject.FindGameObjectsWithTag("UITile");
+        defaultColor = gameObject.GetComponent<Image>().color;
 
         if (canvas.activeInHierarchy)
         {
@@ -28,7 +41,11 @@ public class UITileController : MonoBehaviour
                 chunksInWorld.Add(child.gameObject);
             }
         }
-        
+
+        top = transform.Find("border_top").gameObject;
+        bottom = transform.Find("border_bottom").gameObject;
+        left = transform.Find("border_left").gameObject;
+        right = transform.Find("border_right").gameObject;
     }
 
     // Update is called once per frame
@@ -39,6 +56,22 @@ public class UITileController : MonoBehaviour
             inWorldChunk = SynchroniseChunk();
 
             LoadUIImage();
+        }
+
+        RenderBorders();
+
+        if(gameObject.GetComponent<Image>().sprite == defaultSprite)
+        {
+            RenderInOut(top, false);
+            RenderInOut(bottom, false);
+            RenderInOut(left, false);
+            RenderInOut(right, false);
+
+            gameObject.GetComponent<Image>().color = new Color(0, 0, 100, 0);
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().color = defaultColor;
         }
     }
 
@@ -68,4 +101,53 @@ public class UITileController : MonoBehaviour
         }
         
     }
+    void RenderBorders()
+    {
+        /*//Chunk above
+        Chunk chunkAbove = new Chunk(thisChunk.x, thisChunk.z + 1);
+        //Chunk below
+        Chunk chunkBelow = new Chunk(thisChunk.x, thisChunk.z - 1);
+        //Chunk left
+        Chunk chunkLeft = new Chunk(thisChunk.x - 1, thisChunk.z);
+        //Chunk left
+        Chunk chunkRight = new Chunk(thisChunk.x + 1, thisChunk.z);*/
+
+        foreach (GameObject tile in uiTiles)
+        {
+            Chunk selectedChunk = tile.GetComponent<Chunk>();
+            if (selectedChunk.x == thisChunk.x && selectedChunk.z == thisChunk.z + 1 && tile.GetComponent<Image>().sprite != defaultSprite)
+            {
+                //There is a laoded chunk above this chunk, so dont render the top border
+                RenderInOut(top, false);
+            }
+            if (selectedChunk.x == thisChunk.x && selectedChunk.z == thisChunk.z - 1 && tile.GetComponent<Image>().sprite != defaultSprite)
+            {
+                //There is a laoded chunk below this chunk, so dont render the bottom border
+                RenderInOut(bottom, false);
+            }
+            if (selectedChunk.x == thisChunk.x - 1 && selectedChunk.z == thisChunk.z && tile.GetComponent<Image>().sprite != defaultSprite)
+            {
+                //There is a laoded chunk left of this chunk, so dont render the left border
+                RenderInOut(left, false);
+            }
+            if (selectedChunk.x == thisChunk.x + 1 && selectedChunk.z == thisChunk.z && tile.GetComponent<Image>().sprite != defaultSprite)
+            {
+                //There is a laoded chunk right of this chunk, so dont render the right border
+                RenderInOut(right, false);
+            }
+        }
+
+    }
+    void RenderInOut(GameObject border, bool render)
+    {
+        if (render == true)
+        {
+            border.gameObject.SetActive(true);
+        }
+        if (render == false)
+        {
+            border.gameObject.SetActive(false);
+        }
+    }
+
 }
