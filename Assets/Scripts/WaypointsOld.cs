75d59e6ece647e56980f4df5cc2ad33b923a83b8
AsBas <90893787+sappieman@users.noreply.github.com>
2023-01-18 12:10:51 +0100

    kid walking

--- Assets/Scripts/WaypointsOld.cs
+++ Assets/Scripts/WaypointsOld.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Waypoints : MonoBehaviour
 {
+    public float size;
     public float speed = 1;
     private Transform target;
     private int waypointindex = 0;
     void Start()
     {
-        target = GoToPoint.points[0];
+       // target = GoToPoint.points[0];
     }
 
     // Update is called once per frame
@@ -25,6 +27,7 @@ public class Waypoints : MonoBehaviour
     }
     void GetNextWaypoint()
     {
+        /*
         if (waypointindex == GoToPoint.points.Length - 1)
         {
 
@@ -33,6 +36,12 @@ public class Waypoints : MonoBehaviour
         }
         waypointindex++;
         target = GoToPoint.points[waypointindex];
+        */
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, size);
     }
 
 }
